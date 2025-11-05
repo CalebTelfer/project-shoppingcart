8a89816735af6f6c03eaa68dcce9b4e661fd1955
caleb <calebtelfe@gmail.com>
2025-11-05 02:02:25 -0500

    basic nav. (trying mobile first this time)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import { Link } from "react-router";
 import "../pages/homePage.css"
 
+import store from "../assets/store.svg"
+import cart from "../assets/cart.svg"
+
 function Navbar() {
 
   return (
     <>
         <nav>
+            <img className="nav-svg" src={store} alt="Store svg" />
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="Shop">Shop</Link></li>
                 <li><Link to="Cart">Cart</Link></li>
             </ul>
+            <img className="nav-svg" src={cart} alt="Store svg" />
         </nav>
     </>
   );
